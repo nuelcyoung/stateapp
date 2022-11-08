601d0f97b505a243a61e3cce0f10e876a49a244a
Nuel Young Chukwunalu <nuelmega@gmail.com>
2022-11-08 11:46:57 +0100

    new

--- lib/widgets/profile_widget.dart
+++ lib/widgets/profile_widget.dart
@@ -11,71 +11,81 @@ class _ProfileWidgetState extends State<ProfileWidget> {
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     return Container(
-      color: Colors.white,
+      color: Colors.indigo[100],
+      padding: const EdgeInsets.only(left:15.0,top: 15.0,right: 15.0),
       child: Stack(
         children: [
-          Container(
-            color: Colors.indigo[100],
-            height: screenHeight * 0.5,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: Image.network(
-                        'https://cdn.pixabay.com/photo/2017/08/01/08/29/woman-2563491_960_720.jpg',
-                        height: 200.0,
-                      )),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                const Text(
-                  "Stephanie Milton",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
-                ),
-                const SizedBox(
-                  height: 20.0,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  width: 150,
-                  height: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.blue[400],
-                      borderRadius: BorderRadius.circular(25)),
-                  child: const Text(
-                    "Favorite",
-                    textAlign: TextAlign.center,
+          Positioned(
+            width: screenWidth * 1.0,
+            top: 0,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(500),
+                        child: Image.network(
+                          'https://cdn.pixabay.com/photo/2017/08/01/08/29/woman-2563491_960_720.jpg',
+                          height: 200.0,
+                        )),
                   ),
-                )
-              ],
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  const Text(
+                    "Stephanie Milton",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    width: 150,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.circular(25)),
+                    child: const Text(
+                      "Favorite",
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           Positioned(
             bottom: 0,
             child: Container(
+              width: screenWidth * 1.0,
+              height: screenHeight * 0.4,
               decoration: const BoxDecoration(
+                color: Colors.white,
                 borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40),
-                    topRight: Radius.circular(40)),
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
               ),
-              color: Colors.blue,
-              height: screenHeight * 0.5,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  ListTile(
-                    title: const Icon(Icons.message),
-                    leading: const Text(
-                      "Start a Chat",
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  Container(
+                    padding: const EdgeInsets.all(30),
+                    color: Colors.white,
+                    child: ListTile(
+                      title: const Text(
+                        "Start a Chat",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      leading: const Icon(Icons.message),
+                      trailing: IconButton(
+                          onPressed: (() {}), icon: const Icon(Icons.forward)),
                     ),
-                    trailing: IconButton(
-                        onPressed: (() {}), icon: const Icon(Icons.forward)),
                   ),
                 ],
               ),
